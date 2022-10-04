d3d693b9b2aa521c5349829fe9b3743b0e973f31
ldhapple <lucky77795@naver.com>
2022-10-04 22:46:22 +0900

    init

---
+++ src/main/java/Dohyun/Webtoon_recommender/WebtoonRecommenderApplication.java
@@ -0,0 +1,13 @@
+package Dohyun.Webtoon_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtoonRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtoonRecommenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dohyun/Webtoon_recommender/WebtoonRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package Dohyun.Webtoon_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtoonRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
