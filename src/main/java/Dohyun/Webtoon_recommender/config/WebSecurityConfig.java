92b0671059736c1d3b1217fb997ac416976b975d
ldhapple <lucky77795@naver.com>
2022-10-19 15:22:47 +0900

    horizon_scroll

--- src/main/java/Dohyun/Webtoon_recommender/config/WebSecurityConfig.java
+++ src/main/java/Dohyun/Webtoon_recommender/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/account/register", "/css/**","/img/**").permitAll()
+                        .antMatchers("/", "/account/register", "/css/**", "/img/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
